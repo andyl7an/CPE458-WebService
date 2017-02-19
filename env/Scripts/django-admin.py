c8c2058219c016a720ea201318e363c90da669f6
andyl7an <atan12@calpoly.edu>
2017-02-18 20:42:36 -0800

    The first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\imeani~1\docume~1\andyyz~1\calpoly\cpe458\webser~1\my_fir~1\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
